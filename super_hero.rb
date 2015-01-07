e94892dc9321b50936c56c533f818be4515d784d
CestLou <alisonrebeccarobb@gmail.com>
2015-01-06 16:09:49 -0800

    Inital commit. First problem completed

---
+++ super_hero.rb
@@ -0,0 +1,18 @@
+# SuperHero
+
+# Create a class called SuperHero that takes a firstName and lastName. Give each superHero a superPunch method that returns "WHAM!".
+
+class SuperHero
+	attr_accessor :firstName, :lastName
+	def initialize firstName, lastName
+		@firstName = firstName
+		@lastName = lastName
+	end
+	def superPunch
+		return "WHAM!"
+	end
+
+end
+
+super_man = SuperHero.new "Clark","Kent"
+p super_man.superPunch
